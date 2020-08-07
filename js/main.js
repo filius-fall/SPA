75b6ae988e9832466ad795db6027f503deca833d
rams <filius-fall>
2020-08-07 13:31:47 +0530

    Basic structure

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// // window.addEventListener("hashchange",function(){
+// //     console.log(location.hash);
+// // });
+// function getContent(x){
+//     var items={
+//         home:"this is home page",
+//         about:"Thid is about page",
+//         contact:"this is contact page",
+//     };
+//     return items[x];
+// }
+// function loadContent(){
+//     var contentDiv = document.getElementById("app");
+//     name=location.hash.substr(1);
+//     contentDiv.innerHTML = getContent(name);
+// }
+
+// if(!location.hash){
+//     location.hash="#home";
+// }
+// loadContent();
+// window.addEventListener("hashchange", loadContent)
