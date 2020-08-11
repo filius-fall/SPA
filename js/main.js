785b0887489cb39b28b928367976788df92ba306
rams <filius-fall>
2020-08-11 20:25:27 +0530

    added cart

--- js/main.js
+++ js/main.js
@@ -25,29 +25,113 @@
 
 let carts = document.querySelectorAll(".add-cart")
 
+
+let products = [
+    
+
+    {
+        name:'Arduino Uno',
+        tag:'arduino',
+        price:500,
+        inCart:0,
+    },
+
+    {
+        name:'Relay',
+        tag:'relay',
+        price:150,
+        inCart:0,
+    },
+
+    {
+        name:'Raspberry pi',
+        tag:'raspberrypi',
+        price:3500,
+        inCart:0,
+    },
+
+    {
+        name:'Node MCU',
+        tag:'nodemcu',
+        price:200,
+        inCart:0,
+    },
+
+    
+];
+    
+
 for(let i=0;i<carts.length;i++){
     carts[i].addEventListener('click',() => {
-        cartsNumber();
+        cartsNumber(products[i]);
+        totalCost(products[i])
     })
 }
 
 
-function cartsNumber(){
-    let ProductNumber = parseInt(localStorage.getItem('cartNumber'))
+
+
+function cartsNumber(product){
+    console.log("your items are",product);
+    let ProductNumber = parseInt(localStorage.getItem('cartNumber'));
     if(ProductNumber){
         localStorage.setItem('cartNumber',ProductNumber+1);
         document.querySelector('.cart span').textContent=ProductNumber+1;
-    }
-    else{
+    }else{
         localStorage.setItem('cartNumber',1);
         document.querySelector('.cart span').textContent=1;
     }
+    setItems(product);
+}
+
+function setItems(product){
+    console.log("you are now inside setItmes");
+    let cartItems = localStorage.getItem('productInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if (cartItems != null){
+        console.log("you are inside not null");
+        if (cartItems[product.tag] == undefined){
+            console.log("you are inside undefined");
+            cartItems={
+                ...cartItems,
+                [product.tag]:product  
+            }
+            console.log("cartItems",cartItems);
+        }
+        console.log("you are outside undefined")
+        cartItems[product.tag].inCart += 1;
+    } else {
+        console.log("1")
+        product.inCart = 1;
+
+        cartItems = {
+            [product.tag]:product
+        }
+        
+    }
+    
+    localStorage.setItem('productInCart',JSON.stringify(cartItems));
 }
 
 function realcart(){
     let x = localStorage.getItem('cartNumber')
     if(x){
         document.querySelector('.cart span').textContent=x;
+    }
+
+}
+
+function totalCost(product){
+    console.log("The Product Proce is:",product.price)
+    let cartCost = localStorage.getItem("totalcost",product.price);
+
+    if (cartCost != null){
+        cartCost = parseInt(localStorage.getItem("totalcost",product.price));
+        localStorage.setItem("totalcost", cartCost + product.price);
+        console.log(typeof(product.price));
+    } else{
+        localStorage.setItem("totalcost", product.price);
     }
 
 }
